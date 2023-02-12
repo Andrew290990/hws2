ddcf4ddbf4abf27aa32c809b6694e3f62cc29fd6
Андрей <Ands2909902@gmail.com>
2023-02-12 23:41:12 +0300

    2

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
 import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
+import { Hidden } from '@mui/material'
 
 /*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
-* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
+//* 1 - описать типы AffairPriorityType, AffairType
+//* 2 - указать нужный тип для defaultAffairs
+* 3 - дописать типы и логику функции filterAffairs и роверить её тестамип
 * 4 - выполнить пункт 3 для функции deleteAffair
 * 5 - указать нужный тип в useState с affairs
 * 6 - дописать тип и логику функции deleteAffairCallback
@@ -17,41 +18,70 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = 'high'| 'low'|'middle' // need to fix any
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number // need to fix any
+    name: string // need to fix any
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+let defaultAffairs:Array<AffairType> = [ // need to fix any
     {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
     {_id: 4, name: 'work', priority: 'high'},
     {_id: 5, name: 'html & css', priority: 'middle'},
+    
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any
+    
 
-
-    return affairs // need to fix
+    return filter === "all" ? affairs : affairs.filter(el=> el.priority === filter)
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
+ export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
+       return affairs.filter(el => el._id !== _id)
+    
+    
+ }
 
-    return affairs // need to fix
-}
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
-    const [filter, setFilter] = useState<FilterType>('all')
+    let [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs) // need to fix any
+    let [filter, setFilter] = useState<FilterType>('all')
 
-    const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
+    let filteredAffairs = filterAffairs(affairs, filter)
+    
+
+//- - - - -- - - - - - -- - - - -- -- -- - - - - - - - - - - -
+   
+    
+    // if(filter ==='high'){
+    //     filteredAffairs = affairs.filter(el=> el.priority === 'high')
+    // }
+    // if(filter ==='middle'){
+    //     filteredAffairs = affairs.filter(el=> el.priority === 'middle')
+    // }
+    // if(filter ==='low'){
+    //     filteredAffairs= affairs.filter(el=> el.priority === 'low')
+    // }
+
+  
+   
+   // = - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+ 
+
+    let deleteAffairCallback = (_id:number) => { // need to fix any
+        
+        setAffairs(deleteAffair(affairs,_id))
+    
+        
+            
     }
 
     return (
@@ -60,9 +90,15 @@ function HW2() {
             <div className={s2.hw}>
                 <Affairs
                     data={filteredAffairs}
-                    setFilter={setFilter}
+                    
                     deleteAffairCallback={deleteAffairCallback}
+                    
+                    setFilter={setFilter}
+                    
                     filter={filter}
+
+
+                    
                 />
             </div>
         </div>
