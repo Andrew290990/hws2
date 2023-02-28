22593e9e6880673b221c5dbea928f5ae3087eb7a
Андрей <Ands2909902@gmail.com>
2023-02-28 11:43:59 +0300

    4

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -80,7 +80,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-with-text'}
                         checked={stateForAllCheckboxes}
-                        onChangeChecked={setChecked}
+                        onChange={(e) => setChecked(e.currentTarget.checked)}
                     >
                         some text
                     </SuperCheckbox>
@@ -90,7 +90,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChangeChecked={setChecked}
+                        onChange={(e) => setChecked(e.currentTarget.checked)}
                     />
                 </div>
             </div>
